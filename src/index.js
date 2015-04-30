c9fa41d031b9d3ebdb8716233482cdaf25add259
Chris Cowan <agentme49@gmail.com>
2015-04-29 22:06:20 -0700

    stuff works

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+export default function autoHtml(templateParts, ...values) {
+  const parts = new Array(templateParts.length*2-1);
+  parts[0] = templateParts[0];
+  for (let i=0, len=values.length; i<len; i++) {
+    const value = values[i];
+    parts[2*i+1] = value.__html ? value.__html : _.escape(value);
+    parts[2*i+2] = templateParts[i+1];
+  }
+  return parts.join('');
+}
---
+++ test/main.js
@@ -0,0 +1,24 @@
+import assert from "assert";
+import autoHtml from "../src/";
+
+describe("autoHtml", function() {
+  it("works in basic case", function() {
+    const str = autoHtml `<div>${"\"'foo<>&amp;123"}</div>
+ <span>${"\"'foo<>&amp;123"}</span>`;
+    assert.strictEqual(
+      str,
+      "<div>&quot;&#39;foo&lt;&gt;&amp;amp;123</div>\n " +
+      "<span>&quot;&#39;foo&lt;&gt;&amp;amp;123</span>"
+    );
+  });
+
+  it("works with raw html", function() {
+    const str = autoHtml `<div>${"\"'foo<>&amp;123"}</div>
+ <span>${{__html:"\"'foo<>&amp;123"}}</span>`;
+    assert.strictEqual(
+      str,
+      "<div>&quot;&#39;foo&lt;&gt;&amp;amp;123</div>\n " +
+      "<span>\"'foo<>&amp;123</span>"
+    );
+  });
+});
